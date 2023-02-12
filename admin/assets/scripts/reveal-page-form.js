0341b9d404d9741f2181adf922a26647eb873f6b
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-12 23:55:10 +0530

    adding new page working

--- admin/assets/scripts/reveal-page-form.js
+++ admin/assets/scripts/reveal-page-form.js
@@ -2,18 +2,15 @@ function revealPageForm(pageIndex) {
 	let formSubmitButtonContainer = document.querySelector("#news-submit-button");
 	formSubmitButtonContainer.innerHTML = `<button onclick="submitNews(${pageIndex})" id="submit-news-button" class="add-news-button">Submit</button>`;
 
-	document.getElementById("editor-header-page-no").innerText =
-		"Page " + pageIndex + 1;
+	document.getElementById("editor-header-page-no").innerText = `Page ${
+		pageIndex + 1
+	}`;
 	document.querySelector(".admin-main-right-container").style.display = "block";
 
 	document.getElementById(
 		"add-news-button-container"
 	).innerHTML = `<button id="add-news-button" onclick="addNews(event,${pageIndex})" class="add-news-button">Add News</button>`;
 
-	if (
-		newsPaper[pageIndex] &&
-		newsPaper[pageIndex].news &&
-		newsPaper[pageIndex].news.length > 0
-	)
+	if (newsPaper[pageIndex] && newsPaper[pageIndex].news)
 		renderAccordion(pageIndex, "accordion-container");
 }
